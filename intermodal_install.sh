190fdc5a5d70a2582c1f0e0153c8bcf19d7bdfb6
Zoldur <zoldur99@gmail.com>
2018-06-16 03:17:47 -0700

    Get IP

--- intermodal_install.sh
+++ intermodal_install.sh
@@ -14,7 +14,7 @@ COIN_PORT=11707
 RPC_PORT=11708
 
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -151,7 +151,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
